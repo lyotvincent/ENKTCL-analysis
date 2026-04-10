4af35cb1611509a6ac72bdd026a68596399b7ec1
rzh <rrrzhan@mail.nankai.edu.cn>
2026-04-10 15:07:16 +0000

    Upload processed data

--- download.sh
+++ download.sh
@@ -1,5 +1,7 @@
 mkdir -p ./data
 mkdir -p ./figures
 mkdir -p ./tables
-wget http://secure.bioxai.cn/ENKTCL/NKTCL.zip -O ./data/NKTCL.zip
-gunzip ./data/NKTCL.zip
+wget http://secure.bioxai.cn/ENKTCL/gene_pos.txt -O ./data/gene_pos.txt
+wget http://secure.bioxai.cn/ENKTCL/cellphonedb.zip -O ./data/cellphonedb.zip
+wget http://secure.bioxai.cn/ENKTCL/NKTCL_data-processed.tar.gz -O ./data/NKTCL_data-processed.tar.gz
+tar -xzvf ./data/NKTCL_data-processed.tar.gz -C ./data
